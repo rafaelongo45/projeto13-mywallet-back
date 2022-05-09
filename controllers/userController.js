944a75eabdb70a4e916bac23fac699d1c7785b33
Rafael <rafaelongo45@gmail.com>
2022-05-08 22:15:24 -0300

    chore: adjust code for deploy on heroku

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 import chalk from "chalk";
 import bcrypt from 'bcrypt';
 
+import db from "../db.js";
+
 export async function signUp(req, res){
   const {name, email,password} = req.body;
   try {
